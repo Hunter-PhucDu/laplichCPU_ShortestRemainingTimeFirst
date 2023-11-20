5c5c341656bca0a5bbc050a136700ae76d2e333f
Phuc-Du <hunterofdark352@gmail.com>
2023-11-20 18:32:58 +0700

    commit final

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class CPUSchedulingSimulation extends Component {
     const runStep = async () => {
       if (this.state.processes.some(process => process.time > 0)) {
         if (!isPaused) {
-          // Lọc các tiến trình có Thời gian đến <= currentTime
+          // Filter processes with Arrival Time <= currentTime
           const availableProcesses = this.state.processes.filter(
             process => process.time > 0 && process.arrivalTime <= this.state.currentTime
           );
@@ -50,7 +50,7 @@ class CPUSchedulingSimulation extends Component {
             return;
           }
 
-          // Sắp xếp các tiến trình có sẵn theo CPU Burst Time (thời gian) theo thứ tự tăng dần
+          // Sort available processes by CPU Burst Time (time) in ascending order
           availableProcesses.sort((a, b) => a.time - b.time);
 
           const nextProcess = availableProcesses[0];
@@ -99,16 +99,16 @@ class CPUSchedulingSimulation extends Component {
     }
 
     if (name && time >= 0 && arrivalTime >= 0) {
-      // Tạo một id duy nhất cho tiến trình
+      // Generate a unique id for the process
       const id = Date.now();
 
-      // kiểm tra xem tên tiến trình này đã xuất hiện trong danh sách chưa
+      // Check if the name already exists in the processes
       if (this.state.initialProcesses.some(process => process.name === name)) {
         alert('Tên tiến trình đã tồn tại. Vui lòng chọn tên khác.');
         return;
       }
 
-      // Cập nhật state với process mới
+      // Update state with the new process
       this.setState(prevState => ({
         processes: [...prevState.processes, { id, name, time, arrivalTime }],
         initialProcesses: [...prevState.initialProcesses, { id, name, time, arrivalTime }],
